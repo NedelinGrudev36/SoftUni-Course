ca1f5201cebbcb8e3c5f82ad91ded84a990dd189
Nedelin <grudevnedelin31@gmail.com>
2023-06-04 01:43:04 +0300

    methods

---
+++ C# Fundamentals/Lab Methods/Greater of Two Values/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Greater_of_Two_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ C# Fundamentals/Lab Methods/Math Power/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNum = double.Parse(Console.ReadLine());
+           double power = double.Parse(Console.ReadLine());
+         double result = 0;
+             result = RaiseToPower(baseNum, power,result);
+            Console.WriteLine(result);
+
+        }
+         static double RaiseToPower(double baseNum,double power,double result)
+         {
+
+            result += baseNum;
+            for (int i = 1; i <power; i++)
+            {
+                result *= baseNum;
+            }
+            
+            return result;
+         }
+    }
+}
