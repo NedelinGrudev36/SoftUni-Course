2e438afbd083cc0af0d1fe3281a16554220fd4c7
Nedelin <grudevnedelin31@gmail.com>
2023-06-05 20:31:28 +0300

    method lab more

---
+++ C# Fundamentals/Lab Methods/Math Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = int.Parse(Console.ReadLine());
+            string a = Console.ReadLine();
+            int numberB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculator(numberA, a, numberB));
+
+        }
+
+        static double Calculator(int numberA, string a, int numberB)
+        {
+            double result = 0;
+            if (a == "/")
+            {
+                result = numberA / numberB;
+            }
+            else if (a == "*")
+            {
+                result = numberA * numberB;
+            }
+            else if (a == "+")
+            {
+                result = numberA + numberB;
+            }
+            else if (a == "-")
+            {
+                result = numberA - numberB;
+            }
+
+            return result;
+        }
+    }
+}
