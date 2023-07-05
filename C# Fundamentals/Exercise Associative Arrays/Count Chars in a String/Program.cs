d700350c43afc20b0ee67f39f843464a90b0f6a1
Nedelin <grudevnedelin31@gmail.com>
2023-07-06 01:31:11 +0300

    exercise

---
+++ C# Fundamentals/Exercise Associative Arrays/Count Chars in a String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> countCharacters = new Dictionary<char, int>();
+            string line = Console.ReadLine();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (countCharacters.ContainsKey(line[i]))
+                {
+                    countCharacters[line[i]]++;
+                }
+                else
+                {
+                    countCharacters.Add(line[i], 1);
+                }
+                //countCharacters.Add(line[0],)
+            }
+            foreach (var item in countCharacters.Keys)
+            {
+                Console.WriteLine($"{item} -> {countCharacters[item]}");
+            }
+        }
+    }
+}
