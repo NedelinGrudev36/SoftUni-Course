cd348ec9effc89a4e38d18d9d895fa2ca430dce0
Nedelin <grudevnedelin31@gmail.com>
2023-07-15 00:11:46 +0300

    Regular Expressions Lab

---
+++ C# Fundamentals/Lab Regular Expressions CSharp/Match Full Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            MatchCollection m = Regex.Matches(input, pattern);
+            foreach (Match name in m)
+            {
+                Console.Write(name.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Fundamentals/Lab Regular Expressions CSharp/Match Phone Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"'\b\+359 2 [1-9]{3} [1-9]{4}|\+359-2-[1-9]{3}-[1-9]{4}"
+            string phone = Console.ReadLine();
+            MatchCollection phoneMatches = Regex.Matches(phone, regex);
+            var matchesPhone = phoneMatches
+                .Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+            Console.WriteLine(string.Join(", ",matchesPhone));
+        }
+    }
+}
