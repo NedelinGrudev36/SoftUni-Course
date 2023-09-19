ab4e5b698be8b1900b169a835eafee24ad37cb5e
Nedelin <grudevnedelin31@gmail.com>
2023-09-19 23:17:56 +0300

    ..

---
+++ Advanced with C#/C# Advanced - септември 2023/Multidimensional Arrays/sumMatrixElement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumMatrixElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ Advanced with C#/C# Advanced - септември 2023/Stack and Queue Exercise/Basic Queue Operations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int valuesToPush = values[0];
+            int valuesToPop = values[1];
+            int lookUpValue = values[2];
+
+            Queue<int> stack = new Queue<int>(input.Take(valuesToPush));
+
+            
+
+            while (stack.Count > 0 && valuesToPop > 0)
+            {
+                stack.Dequeue();
+                valuesToPop--;
+            }
+
+            if (stack.Contains(lookUpValue))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine("false");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
