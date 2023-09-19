bb0c069fe4250de2d512a0106826e74e5d11b64f
Nedelin <grudevnedelin31@gmail.com>
2023-09-19 18:15:14 +0300

    .

---
+++ Advanced with C#/C# Advanced - септември 2023/Stack and Queue Exercise/Basic Stack Operations/Basic Stack Operations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            
+            
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+
+            int valuesToPush = values[0];
+            int valuesToPop = values[1];
+            int lookUpValues = values[2];
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < valuesToPush; i++)
+            {
+                stack.Push(input[1]);
+            }
+
+            while (stack.Count>0 && valuesToPop>0)
+            {
+                stack.Pop();
+                valuesToPop--;
+            }
+
+            if (stack.Contains(lookUpValues))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count==0)
+            {
+                Console.WriteLine("false");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
