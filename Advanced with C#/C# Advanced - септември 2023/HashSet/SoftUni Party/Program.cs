0da50f3cb3bfffd0a7139fc2f01a4e562e527510
Nedelin <grudevnedelin31@gmail.com>
2023-09-24 22:58:50 +0300

    .

--- Advanced with C#/C# Advanced - септември 2023/HashSet/SoftUni Party/Program.cs
+++ Advanced with C#/C# Advanced - септември 2023/HashSet/SoftUni Party/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = Console.ReadLine();
+HashSet<string> allPeople = new HashSet<string>();
+
+while(input!="PARTY")
+{
+   allPeople.Add(input);
+    input = Console.ReadLine();
+}
+while (input!="END")
+{
+	allPeople.Remove(input);
+	input = Console.ReadLine();
+}
+Console.WriteLine(allPeople.Count);
+foreach (string person in allPeople.OrderByDescending(x => char.IsDigit(x[0])))
+{
+	Console.WriteLine(person);
+}
