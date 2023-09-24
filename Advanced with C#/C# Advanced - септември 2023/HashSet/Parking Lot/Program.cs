630de15ba79dfc4791f7340a5f1ee1b3472fae0d
Nedelin <grudevnedelin31@gmail.com>
2023-09-24 22:10:36 +0300

    HashSet

---
+++ Advanced with C#/C# Advanced - септември 2023/HashSet/Parking Lot/Program.cs
@@ -0,0 +1,28 @@
+﻿string input = Console.ReadLine();
+
+HashSet<string> parking = new HashSet<string>();
+while (input!="END")
+{
+    string direction = input.Split(", ")[0];
+    string carNumber = input.Split(", ")[1];
+
+    if (direction=="IN")
+    {
+        parking.Add(carNumber);
+    }
+    else
+    {
+        parking.Remove(carNumber); 
+    }
+
+
+    input = Console.ReadLine();
+}
+if (parking.Count<=0)
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
+foreach (string item in parking)
+{
+    Console.WriteLine(item);
+}
---
+++ Advanced with C#/C# Advanced - септември 2023/HashSet/Record Unique Names/Record Unique Names/Program.cs
@@ -0,0 +1,14 @@
+﻿int count = int.Parse(Console.ReadLine());
+HashSet<string> names = new HashSet<string>();
+for (int i = 0; i < count; i++)
+{
+    string name=Console.ReadLine();
+    if (!names.Contains(name))
+    {
+        names.Add(name);
+    }
+}
+foreach (string item in names)
+{
+    Console.WriteLine(item);
+}
