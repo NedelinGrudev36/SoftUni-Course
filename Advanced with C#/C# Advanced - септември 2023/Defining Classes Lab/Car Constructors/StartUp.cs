e8d518578ea24772a8fcf430b64809a67c31e0f7
Nedelin <grudevnedelin31@gmail.com>
2023-09-30 21:37:37 +0300

    exercise

---
+++ Advanced with C#/C# Advanced - септември 2023/Defining Classes Lab/Car Constructors/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+
+        static void Main()
+        {
+            string make=Console.ReadLine();
+            string model=Console.ReadLine();
+            int year=int.Parse(Console.ReadLine());
+            int fuelQuantity=int.Parse(Console.ReadLine());    
+            int fuelConsumption = int.Parse(Console.ReadLine());    
+
+            Car car = new Car();
+            Car carSecond = new Car(make,model,year);
+            Car carThird = new Car(make,model,year,fuelQuantity,fuelConsumption);
+            
+            
+
+        
+
+
+        }
+
+
+    }
+
+}
