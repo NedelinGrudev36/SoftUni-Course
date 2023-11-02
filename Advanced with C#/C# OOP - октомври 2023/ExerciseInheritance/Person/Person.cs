cf4964c6c6e474ee247115341fa5f7722a3338ae
Nedelin <grudevnedelin31@gmail.com>
2023-11-02 23:07:30 +0200

    upr

---
+++ Advanced with C#/C# OOP - октомври 2023/ExerciseInheritance/Person/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"Name: {this.Name}, Age: {this.Age}");
+            return stringBuilder.ToString();
+        }
+
+
+      
+    }
+}
---
+++ Advanced with C#/C# OOP - октомври 2023/ExerciseInheritance/playersAndMonsters/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace playersAndMonsters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
