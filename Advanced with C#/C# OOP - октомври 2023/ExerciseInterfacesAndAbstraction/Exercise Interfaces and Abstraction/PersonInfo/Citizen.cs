2b70981d0a43d50056ce2e3f754fb1fb70d076bc
Nedelin <grudevnedelin31@gmail.com>
2023-11-15 22:17:30 +0200

    exercise

---
+++ Advanced with C#/C# OOP - октомври 2023/ExerciseInterfacesAndAbstraction/Exercise Interfaces and Abstraction/PersonInfo/Citizen.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson,IBirthable,IIdentifiable
+    {
+
+        public Citizen(string name,int age,string id,string birthable)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthable;
+            this.Id = id;
+        }
+
+        public string Name { get; set; } 
+        public int Age {  get; set; }
+        public string Birthdate { get; set; }
+        public string Id { get; set; }
+    }
+}
