a4b5b827b8cf5813635533b66bc8e925b237c641
Nedelin <109604989+NedelinGrudev36@users.noreply.github.com>
2024-05-20 20:06:35 +0300

    .

--- MS SQL/Exercise Databases Introduction/Exercise1-24.sql
+++ MS SQL/Exercise Databases Introduction/Exercise1-24.sql
@@ -170,69 +170,218 @@ SELECT * FROM Movies
 CREATE DATABASE CarRental
 
 USE CarRental
-CREATE TABLE Categories
+
+CREATE TABLE [Categories]
 (
-	Id INT PRIMARY KEY IDENTITY,
-	CategoryName VARCHAR(30),
-	DailyRate INT,
-	WeeklyRate INT,
-	MonthlyRate INT,
-	WeekendRate INT
+	[Id] INT PRIMARY KEY IDENTITY,
+	[CategoryName] VARCHAR(50) NOT NULL,
+	[DailyRate] DECIMAL(6, 2) NOT NULL,
+	[WeeklyRate] DECIMAL(6, 2) NOT NULL,
+	[MonthlyRate] DECIMAL(6, 2) NOT NULL,
+	[WeekendRate] DECIMAL(6, 2) NOT NULL
 )
 
-CREATE TABLE Cars
+CREATE TABLE [Cars]
 (
-	Id INT PRIMARY KEY IDENTITY,
-	PlateNumber INT,
-	Manufacturer VARCHAR(30),
-	Model VARCHAR(30),
-	CarYear VARCHAR(30),
-	CategoryId INT,
-	Doors INT,
-	Picture VARBINARY(MAX),
-	Condition VARCHAR(30),
-	Available VARCHAR(30)
+	[Id] INT PRIMARY KEY IDENTITY,
+	[PlateNumber] VARCHAR(30) NOT NULL,
+	[Manufacturer] VARCHAR(50) NOT NULL,
+	[Model] VARCHAR(50) NOT NULL,
+	[CarYear] INT NOT NULL,
+	[CategoryId] INT FOREIGN KEY REFERENCES [Categories](Id) NOT NULL,
+	[Doors] INT NOT NULL,
+	[Picture] IMAGE,
+	[Condition] NVARCHAR(1000) NOT NULL,
+	[Available] BIT NOT NULL
 )
 
-CREATE TABLE Employees 
+CREATE TABLE [Employees]
 (
-	Id INT PRIMARY KEY IDENTITY,
-	FirstName VARCHAR(30),
-	LastName VARCHAR(30),
-	Title VARCHAR(30),
-	Notes VARCHAR(MAX)
+	[Id] INT PRIMARY KEY IDENTITY,
+	[FirstName] VARCHAR(30) NOT NULL,
+	[LastName] VARCHAR(30) NOT NULL,
+	[Title] VARCHAR(50) NOT NULL,
+	[Notes] NVARCHAR(1000)
 )
 
-CREATE TABLE Customers
+CREATE TABLE [Customers]
 (
-	Id INT PRIMARY KEY IDENTITY,
-	DriverLicenceNumber INT,
-	FullName VARCHAR(MAX),
-	[Address] VARCHAR(50),
-	City VARCHAR(30),
-	ZIPCode INT,
-	Notes VARCHAR(30)
+	[Id] INT PRIMARY KEY IDENTITY,
+	[DriverLicenceNumber] INT NOT NULL,
+	[FullName] VARCHAR(50) NOT NULL,
+	[Address] VARCHAR(200) NOT NULL,
+	[City] VARCHAR(50) NOT NULL,
+	[ZIPCode] INT NOT NULL,
+	[Notes] NVARCHAR(1000)
 )
 
-CREATE TABLE RentalOrders
+CREATE TABLE [RentalOrders]
 (
-	Id INT PRIMARY KEY IDENTITY,
-	EmployeeId INT,
-	CustomerId INT,
-	CarId INT,
-	TankLevel INT,
-	KilometrageStart INT,
-	KilometrageEnd INT,
-	TotalKilometrage INT,
-	StartDate DATETIME2,
-	EndDate DATETIME2,
-	TotalDays INT,
-	RateApplied INT,
-	TaxRate INT,
-	OrderStatus VARCHAR(30),
-	Notes VARCHAR(MAX)
+	[Id] INT PRIMARY KEY IDENTITY,
+	[EmployeeId] INT FOREIGN KEY REFERENCES [Employees](Id) NOT NULL,
+	[CustomerId] INT FOREIGN KEY REFERENCES [Customers](Id) NOT NULL,
+	[CarId] INT FOREIGN KEY REFERENCES [Cars](Id) NOT NULL,
+	[TankLevel] INT NOT NULL,
+	[KilometrageStart] INT NOT NULL,
+	[KilometrageEnd] INT NOT NULL,
+	[TotalKilometrage] INT NOT NULL,
+	[StartDate] DATE NOT NULL,
+	[EndDate] DATE NOT NULL,
+	[TotalDays] INT NOT NULL,
+	[RateApplied] DECIMAL(6, 2) NOT NULL,
+	[TaxRate] DECIMAL(4, 2) NOT NULL,
+	[OrderStatus] VARCHAR(50) NOT NULL,
+	[Notes] NVARCHAR(1000)
 )
 
+INSERT INTO [Categories] VALUES
+	('First category name', 10.00, 50.00, 150.00, 20.00),
+	('Second category name', 50.00, 250.00, 750.00, 100.00),
+	('Third category name', 100.00, 500.00, 1500.00, 200.00)
+
+INSERT INTO [Cars] VALUES
+	('PLN 0001', 'Ford', 'Model A', 1994, 1, 4, NULL, 'Good', 1),
+	('PLN 0002', 'Tesla', 'Model B', 2021, 2, 4, NULL, 'Great', 1),
+	('PLN 0003', 'Capsule Corp', 'Model C', 2054, 3, 10, NULL, 'Best', 0)
+
+INSERT INTO [Employees] VALUES
+	('Tyler', 'Durden', 'Edward Norton`s Alter Ego', NULL),
+	('Plain', 'Jane', 'some gal', NULL),
+	('Average', 'Joe', 'some dude', NULL)
+
+INSERT INTO [Customers] VALUES
+	('123456', 'Jimmy Carr', 'Britain', 'London', 1000, NULL),
+	('654321', 'Bill Burr', 'USA', 'Washington', 2000, NULL),
+	('999999', 'Louis CK', 'Mexico', 'Mexico City', 3000, NULL)
+
+INSERT INTO [RentalOrders] VALUES
+	(1, 1, 1, 70, 90000, 100000, 10000, '1994-10-01', '1994-10-21', 20, 100.00, 14.00, 'Pending', NULL),
+	(2, 2, 2, 85, 250000, 2750000, 25000, '2011-11-12', '2011-11-24', 12, 150.00, 17.50, 'Canceled', NULL),
+	(3, 3, 3, 90, 0, 120000, 120000, '2025-04-05', '2025-05-02', 27, 220.00, 21.25, 'Delivered', NULL)
+
+	--15. Hotel Database
+	CREATE DATABASE Hotel
+	USE Hotel
+
+	CREATE TABLE [Employees]
+(
+	[Id] INT PRIMARY KEY IDENTITY,
+	[FirstName] VARCHAR(50) NOT NULL,
+	[LastName] VARCHAR(50) NOT NULL,
+	[Title] VARCHAR(50) NOT NULL,
+	[Notes] NVARCHAR(1000)
+)
+
+CREATE TABLE [Customers]
+(
+	[AccountNumber] INT PRIMARY KEY IDENTITY,
+	[FirstName] VARCHAR(50) NOT NULL,
+	[LastName] VARCHAR(50) NOT NULL,
+	[PhoneNumber] INT NOT NULL,
+	[EmergencyName] VARCHAR(50),
+	[EmergencyNumber] INT,
+	[Notes] NVARCHAR(1000)
+)
+
+CREATE TABLE [RoomStatus]
+(
+	[RoomStatus] VARCHAR(50) PRIMARY KEY NOT NULL,
+	[Notes] NVARCHAR(1000)
+)
+
+CREATE TABLE [RoomTypes]
+(
+	[RoomType] VARCHAR(50) PRIMARY KEY NOT NULL,
+	[Notes] NVARCHAR(1000)
+)
+
+CREATE TABLE [BedTypes]
+(
+	[BedType] VARCHAR(50) PRIMARY KEY NOT NULL,
+	[Notes] NVARCHAR(1000)
+)
+
+
+CREATE TABLE [Rooms]
+(
+	[RoomNumber] INT PRIMARY KEY IDENTITY,
+	[RoomType] VARCHAR(50) FOREIGN KEY REFERENCES [RoomTypes](RoomType) NOT NULL,
+	[BedType] VARCHAR(50) FOREIGN KEY REFERENCES [BedTypes](BedType) NOT NULL,
+	[Rate] DECIMAL(5,2) NOT NULL,
+	[RoomStatus] VARCHAR(50) FOREIGN KEY REFERENCES [RoomStatus](RoomStatus) NOT NULL,
+	[Notes] NVARCHAR(1000)
+)
+
+CREATE TABLE [Payments]
+(
+	[Id] INT PRIMARY KEY IDENTITY,
+	[EmployeeId] INT FOREIGN KEY REFERENCES [Employees](Id) NOT NULL,
+	[PaymentDate] DATE NOT NULL,
+	[AccountNumber] INT FOREIGN KEY REFERENCES [Customers](AccountNumber) NOT NULL,
+	[FirstDateOccupied] DATE NOT NULL,
+	[LastDateOccupied] DATE NOT NULL,
+	[TotalDays] INT NOT NULL,
+	[AmountCharged] DECIMAL(6, 2) NOT NULL,
+	[TaxRate] DECIMAL(4, 2) NOT NULL,
+	[TaxAmount] DECIMAL(6, 2) NOT NULL,
+	[PaymentTotal] DECIMAL(6, 2) NOT NULL,
+	[Notes] NVARCHAR(1000)
+)
+
+CREATE TABLE [Occupancies]
+(
+	[Id] INT PRIMARY KEY IDENTITY,
+	[EmployeeId] INT FOREIGN KEY REFERENCES [Employees](Id) NOT NULL,
+	[DateOccupied] DATE NOT NULL,
+	[AccountNumber] INT FOREIGN KEY REFERENCES [Customers](AccountNumber) NOT NULL,
+	[RoomNumber] INT FOREIGN KEY REFERENCES [Rooms](RoomNumber) NOT NULL,
+	[RateApplied] DECIMAL(4, 2) NOT NULL,
+	[PhoneCharge] DECIMAL(4, 2) NOT NULL,
+	[Notes] NVARCHAR(1000)
+)
+
+
+INSERT INTO [Employees] VALUES
+	('Jim', 'McJim', 'Supervisor', NULL),
+	('Jane', 'McJane', 'Cook', NULL),
+	('John', 'McJohn', 'Waiter', NULL)
+		
+INSERT INTO [Customers] VALUES
+	('Mickey', 'Mouse', 12345678, 'Minnie', 11111111, NULL),
+	('Donald', 'Duck', 87654321, 'Daisy', 22222222, NULL),
+	('Scrooge', 'McDuck', 9999999, 'Richie', 33333333, NULL)
+		
+INSERT INTO [RoomStatus] VALUES
+	('Free', NULL),
+	('Occupied', NULL),
+	('No idea', NULL)
+		
+INSERT INTO [RoomTypes] VALUES
+	('Room', NULL),
+	('Studio', NULL),
+	('Apartment', NULL)
+		
+INSERT INTO [BedTypes] VALUES
+	('Big', NULL),
+	('Small', NULL),
+	('Child', NULL)
+		
+INSERT INTO [Rooms] VALUES
+	('Room', 'Big', 15.00, 'Free', NULL),
+	('Studio', 'Small', 12.50, 'Occupied', NULL),
+	('Apartment', 'Child', 10.25, 'No idea', NULL)
+		
+INSERT INTO [Payments] VALUES
+	(1, '2023-02-01', 1, '2023-01-11', '2023-01-14', 3, 250.00, 20.00, 50.00, 300.00, NULL),
+	(2, '2023-02-02', 2, '2023-01-12', '2023-01-15', 3, 199.90, 20.00, 39.98, 239.88, NULL),
+	(3, '2023-02-03', 3, '2023-01-13', '2023-01-16', 3, 330.50, 20.00, 66.10, 396.60, NULL)
+	   	
+INSERT INTO [Occupancies] VALUES
+	(1, '2023-01-01', 1, 1, 20.00, 15.00, NULL),
+	(2, '2023-01-02', 2, 2, 20.00, 12.50, NULL),
+	(3, '2023-01-03', 3, 3, 20.00, 18.90, NULL)
+
+--
 
 
 
