e3e0ac0e427572df8c70ff0a1d32dbf5029e2963
Nedelin <109604989+NedelinGrudev36@users.noreply.github.com>
2024-05-29 00:49:14 +0300

    more exercise

--- MS SQL/Exercises Built-in Functions/Build-in_Function_1-19.sql
+++ MS SQL/Exercises Built-in Functions/Build-in_Function_1-19.sql
@@ -1,3 +1,22 @@
 --01. Find names of all employees by first name
---
+
+SELECT [FirstName], [LastName]
+FROM Employees
+WHERE [FirstName] LIKE 'Sa%'
+
+--02. Find names of all employees by last name
+
+SELECT [FirstName], [LastName]
+FROM Employees
+WHERE [LastName] LIKE '%ei%'
+
+--03. Find First Names of all employees
+
+SELECT [FirstName]
+FROM Employees
+WHERE DepartmentId IN (3,10) AND 
+DATEPART(YEAR,HireDate) BETWEEN 1995 AND 2005
+
+
+
 
