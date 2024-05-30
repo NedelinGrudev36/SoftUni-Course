7f2c443d57a0f98dabe2afff196d5d6671b12f10
Nedelin <109604989+NedelinGrudev36@users.noreply.github.com>
2024-05-30 15:20:54 +0300

    compleate all exercise

--- MS SQL/Exercises Built-in Functions/Build-in_Function_1-19.sql
+++ MS SQL/Exercises Built-in Functions/Build-in_Function_1-19.sql
@@ -17,6 +17,67 @@ FROM Employees
 WHERE DepartmentId IN (3,10) AND 
 DATEPART(YEAR,HireDate) BETWEEN 1995 AND 2005
 
+--04. Find all empoloyees except engineers
+SELECT [FirstName], [LastName]
+FROM Employees
+WHERE [JobTitle] NOT LIKE '%engineer%'
+
+--05. Find Towns with name Length
+
+SELECT [Name] 
+FROM Towns
+WHERE LEN([Name]) BETWEEN 5 AND 6
+ORDER BY [Name]
 
 
+--06. find towns starting with
+
+SELECT [TownID], [Name]
+FROM Towns
+WHERE [Name] LIKE '[mkbe]%'
+ORDER BY [Name]
+
+--07. find towns not starting with
+
+SELECT [TownID], [Name]
+FROM Towns
+WHERE [Name] NOT LIKE '[RBD]%'
+ORDER BY [Name]
+
+--08. Create View Employees Hired After 2000 Year
+
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+SELECT [FirstName], [LastName]
+FROM Employees
+WHERE DATEPART(YEAR,HireDate) > 2000
+
+--09. Length of last name
+
+SELECT [FirstName], [LastName]
+FROM Employees
+WHERE LEN(LastName) = 5
+
+--10. rank Employees by Salary
+
+SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+DENSE_RANK() OVER 
+(PARTITION BY Salary ORDER BY EmployeeID)
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 20000
+ORDER BY Salary DESC
+
+--11. find all emplyees with rank 2
+
+WITH CTE_RankedEmployees AS
+(
+	SELECT EmployeeID, [FirstName], [LastName], [Salary]
+	DENSE_RANK() OVER 
+    (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+	FROM Employees
+	WHERE Salary BETWEEN 10000 AND 20000
+)
+SELECT *
+FROM CTE_RankedEmployees
+WHERE [Rank] = 2
+ORDER BY Salary DESC
 
