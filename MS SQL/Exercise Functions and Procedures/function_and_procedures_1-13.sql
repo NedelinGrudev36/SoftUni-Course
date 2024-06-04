716dbead3261e249391c2da89f73d827a92d075b
Nedelin <109604989+NedelinGrudev36@users.noreply.github.com>
2024-06-04 22:40:34 +0300

    first exercise

--- MS SQL/Exercise Functions and Procedures/function_and_procedures_1-13.sql
+++ MS SQL/Exercise Functions and Procedures/function_and_procedures_1-13.sql
@@ -1,1 +1,12 @@
---01. 
+--01. Employees with Salary SoftUni Above 35000
+CREATE PROCEDURE usp_GetEmployeesSalaryAbove3500
+AS 
+BEGIN
+	SELECT FirstName, LastName
+	FROM Employees
+	WHERE Salary > 35000
+END
+
+EXEC usp_GetEmployeesSalaryAbove3500
+
+--02. Employees with Salary Above Number
