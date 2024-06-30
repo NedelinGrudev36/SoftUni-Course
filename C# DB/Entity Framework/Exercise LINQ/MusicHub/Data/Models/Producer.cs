73329c90a6e1ea950fe4835c85ec07e6281f492e
Nedelin <109604989+NedelinGrudev36@users.noreply.github.com>
2024-07-01 00:58:16 +0300

    new exercise

---
+++ C# DB/Entity Framework/Exercise LINQ/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public Producer()
+        {
+            
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+    }
+}
