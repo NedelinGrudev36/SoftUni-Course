2880acedf9a4957a576684582c15c06ca1b15aa8
Nedelin <109604989+NedelinGrudev36@users.noreply.github.com>
2024-07-03 02:21:57 +0300

    more exrcise on advance query

---
+++ C# DB/Entity Framework/Exercise Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+
+            Console.WriteLine(GetBooksByAgeRestriction(context, "miNor"));
+
+            //DbInitializer.ResetDatabase(db);
+        }
+
+        //02
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            if (!Enum.TryParse(command, true, out AgeRestriction ageRestriction))
+            {
+                return string.Empty;
+            }
+
+            var bookTitle = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => b.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            return string.Join(Environment.NewLine,bookTitle);
+        }
+
+        //03
+
+    }
+}
+
+
