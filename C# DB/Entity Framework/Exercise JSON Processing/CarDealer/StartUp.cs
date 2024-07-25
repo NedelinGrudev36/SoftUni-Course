56b819d489e9e35b47918bb80ec2f225d1c2cff4
Nedelin <grudevnedelin31@gmail.com>
2024-07-25 15:52:14 +0300

    json Processing

---
+++ C# DB/Entity Framework/Exercise JSON Processing/CarDealer/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            //01
+            string suppliersString = File.ReadAllText("../../../Datasets/suppliers.json");
+            Console.WriteLine(ImportSuppliers(suppliersString));
+
+            //02
+            string partsString = File.ReadAllText("../../../Datasets/parts.json");
+            Console.WriteLine(ImportParts(partsString));
+
+            //03
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers =JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var parts = JsonConvert.DeserializeObject<List<Part>>(inputJson);
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}.";
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+
+        }
+    }
+}
